fa56350a72adf3358797bb47ac8f1a278375beec
alimsosa <42209713+alimsosa@users.noreply.github.com>
2022-05-16 13:28:35 -0300

    Add files via upload

---
+++ src/main/java/com/distribuidos/corteelectoral/CorteelectoralApplication.java
@@ -0,0 +1,13 @@
+package com.distribuidos.corteelectoral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorteelectoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorteelectoralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distribuidos/corteelectoral/CorteelectoralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribuidos.corteelectoral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorteelectoralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
