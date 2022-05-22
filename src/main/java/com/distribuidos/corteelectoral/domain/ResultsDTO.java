a732092384c6918eaaecb691f446c53cb3f78def
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-05-22 19:47:27 -0300

    conexiondb

--- src/main/java/com/distribuidos/corteelectoral/domain/ResultsDTO.java
+++ src/main/java/com/distribuidos/corteelectoral/domain/ResultsDTO.java
@@ -11,4 +11,22 @@ public class ResultsDTO {
     private String nombre_partido;
     private Integer cantidadVotos;
     private String status;
+	public String getNombre_partido() {
+		return nombre_partido;
+	}
+	public void setNombre_partido(String nombre_partido) {
+		this.nombre_partido = nombre_partido;
+	}
+	public Integer getCantidadVotos() {
+		return cantidadVotos;
+	}
+	public void setCantidadVotos(Integer cantidadVotos) {
+		this.cantidadVotos = cantidadVotos;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
